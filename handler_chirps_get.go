9e99eb5ccaa1ead980d17a82433d414f9d544c5e
gooneraki <hayiate4@hotmail.com>
2025-10-03 10:42:48 +0300

    Implement author-based chirp retrieval: add GetChirpsByAuthor function and update handler for author ID parsing and error handling

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gooneraki/chirpy-go/internal/database"
 )
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetChirps(r.Context())
+	authorId := r.URL.Query().Get("author_id")
+
+	var dbChirps []database.Chirp
+	var err error
+	if authorId == "" {
+		dbChirps, err = cfg.db.GetChirps(r.Context())
+	} else {
+		authorUUID, parseErr := uuid.Parse(authorId)
+		if parseErr != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", parseErr)
+			return
+		}
+		dbChirps, err = cfg.db.GetChirpsByAuthor(r.Context(), authorUUID)
+	}
+
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
 		return
